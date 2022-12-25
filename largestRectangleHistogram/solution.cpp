a4a0839843305e8af279c10276299fd1c6efeb00
Mansi joshi <joshimansi32@gmail.com>
2022-12-25 10:52:12 +0530

    Update solution.cpp

--- largestRectangleHistogram/solution.cpp
+++ largestRectangleHistogram/solution.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
 
-    // function to find the previous smallest element's index
-    vector<int> PreviousSmallest(vector<int>& heights){
-        vector<int> result(heights.size());
-        stack<int> st;
-        for(int i=0;i<heights.size();i++){
-            // remove the index till we find the smaller element's index on left side
-            while(st.empty()==false && heights[i]<=heights[st.top()]) st.pop();
-            // if the stack is empty , there is no smaller element 
-            if(st.empty()) result[i]=-1;
-            else result[i]=st.top();
-            // push the current index into stack
-            st.push(i);
-        }
-        return(result);
-    }
-    
-    // function to find next smallest element's index
-    vector<int> NextSmallest(vector<int>& heights){
-        vector<int> result(heights.size());
-        stack<int> st;
-        for(int i=heights.size()-1;i>-1;i--){
-            // remove the index till we find the smaller element's index on right side
-            while(st.empty()==false && heights[i]<=heights[st.top()]) st.pop();
-            // if the stack is empty , there is no smaller element 
-            if(st.empty()) result[i]=heights.size();
-            else result[i]=st.top();
-            // push the current index into stack
-            st.push(i);
-        }
-        return(result);
-    }
-    
     // function to find largest Rectangle Area
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> PS = PreviousSmallest(heights);
-        vector<int> NS = NextSmallest(heights);
-        int largestArea=0;
-        for(int i=0;i<heights.size();i++){
-            largestArea=max(largestArea,(NS[i]-PS[i]-1)*heights[i]);
-        }
-        return(largestArea);
+         
+       stack<int> st; // create an empty stack
+       int max_area=0; // initializa maximum area
+       int top_st; // it stores the top of stack
+       int top_area; // store the area of the top bar
+       
+       int i=0;
+       while(i<heights.size()){
+       
+         // push the index into stack if stack is empty or the bar is higher than the top of stack
+         if(st.empty() || heights[st.top()] <= heights[i]) st.push(i++);
+         
+         else{
+            top_st = st.top();
+            st.pop();
+            top_area=heights[top_st]*(st.empty()? i: i-st.top()-1);
+            
+            // update the max area
+            max_area=max(max_area,top_area);
+         } 
+      }
+      
+      // pop the remaining bars from stack and calculate the area with every popped bar
+      while(st.empty()==false){
+          top_st=st.top();
+          st.pop();
+          top_area=heights[top_st]*(st.empty()? i: i-st.top()-1);
+          max_area = max(max_area,top_area);
+      }
+      
+      return max_area;
+        
     }
 
 };
